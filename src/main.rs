1ba341a1d9f8361c4e9229763dbd7e9d645a2535
nayan <nayansharma1010@gmail.com>
2023-03-03 16:16:47 +0530

    feat:multithreading with thread pool

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,21 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener,TcpStream},
+    thread,
+    time::Duration,
 };
+
+use web_server::ThreadPool;
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:6969").unwrap();
+    let pool = ThreadPool::new(4);
     
     for stream in listener.incoming() {
         let stream: TcpStream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream)
+        });
     }
 }
 
@@ -18,11 +25,14 @@ fn  handle_connection(mut stream : TcpStream) {
 
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line , filename) = if request_line == "GET / HTTP/1.1" {
+   let (status_line,filename) = match &request_line[..] {
+    "GET / HTTP/1.1" => ("HTTP/1.1 200 Ok" , "fe/index.html"),
+    "GET /sleep HTTP/1.1" => {
+        thread::sleep(Duration::from_secs(20));
         ("HTTP/1.1 200 OK","fe/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND","fe/404.html")
-    };
+    },
+    _ => ("HTTP/1.1 404 NOT FOUND","fe/404.html"),   
+    };      
 
     let contents: String = fs::read_to_string(filename).unwrap();
     let length: usize = contents.len();
